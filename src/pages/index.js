2b5747887f4afe8f19d52e34061bc40ac64133fe
Victoriya Yurlova <yurlovaviktoriya@yandex.ru>
2022-04-13 21:52:46 +0700

    fix: удалить const avatarProfile

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import {
   buttonProfileEdit,
   buttonUpdateAvatar,
   buttonPlaceAdd,
-  profileAvatar,
   settings,
   formValidators
 } from '../utils/constans.js';
--- src/utils/constans.js
+++ src/utils/constans.js
@@ -2,8 +2,6 @@ export const buttonProfileEdit = document.querySelector('.profile__edit-btn');
 export const buttonUpdateAvatar = document.querySelector('.profile__avatar-overlay');
 export const buttonPlaceAdd = document.querySelector('.profile__add-btn');
 
-export const profileAvatar = document.querySelector('.profile__avatar');
-
 export const settings = {
   formSelector: '.form',
   inputSelector: '.form__input',
